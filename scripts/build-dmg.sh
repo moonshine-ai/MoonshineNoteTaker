65fe47d31a3256cca27b82ebb0b07cb2dbbfef8e
Pete Warden <pete@usefulsensors.com>
2026-02-02 11:27:01 -0800

    Build signed archive for .dmg

--- scripts/build-dmg.sh
+++ scripts/build-dmg.sh
@@ -1,34 +1,65 @@
 #!/bin/bash -ex
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+APP_NAME="Moonshine Note Taker"
+
+set -o allexport
+source "$SCRIPT_DIR/../.env"
+set +o allexport
+
 # Build the app in Release mode and copy the result to ~/Downloads/
+ARCHIVE_PATH="$(pwd)/build/MoonshineNoteTaker.xcarchive"
 xcodebuild \
-  -project MoonshineNoteTaker.xcodeproj \
-  -scheme "MoonshineNoteTaker" \
-  -configuration Release \
-  CONFIGURATION_BUILD_DIR="$(pwd)/build" \
-  -derivedDataPath ./build \
-  build
+	-project MoonshineNoteTaker.xcodeproj \
+	-scheme "MoonshineNoteTaker" \
+	-configuration Release \
+	CONFIGURATION_BUILD_DIR="$(pwd)/build" \
+	-derivedDataPath ./build \
+	-archivePath "$ARCHIVE_PATH" \
+	archive
 
-# Find the built .app and copy it to ~/Downloads/
-APP_PATH="build/Moonshine Note Taker.app"
-if [ -d "$APP_PATH" ]; then
-	cp -R "$APP_PATH" ~/Downloads/
-else
-	echo "Moonshine Note Taker.app not found in build/Release."
-	exit 1
-fi
+EXPORT_PATH="$(pwd)/build/export"
 
-rm -rf MoonshineNoteTaker.dmg
+xcodebuild -exportArchive \
+    -archivePath "$ARCHIVE_PATH" \
+    -exportPath "$EXPORT_PATH" \
+    -exportOptionsPlist ExportOptions.plist
+
+APP_PATH="$EXPORT_PATH/$APP_NAME.app"
+
+TMP_DIR=$(mktemp -d)
+
+cp -R "${APP_PATH}" "${TMP_DIR}/${APP_NAME}.app"
+
+rm -rf MoonshineNoteTaker*.dmg
+
+DATE_SUFFIX=$(date +_%Y_%m_%d)
+
+DMG_NAME="MoonshineNoteTaker$DATE_SUFFIX.dmg"
 
 create-dmg \
-	--volname "Moonshine Note Taker" \
+	--volname "$APP_NAME" \
 	--volicon "images/dmg-icon.png" \
 	--background "images/dmg-background.png" \
 	--window-pos 200 120 \
 	--window-size 600 400 \
 	--icon-size 100 \
-	--icon "Moonshine Note Taker.app" 175 190 \
-	--hide-extension "Moonshine Note Taker.app" \
+	--icon "$APP_NAME.app" 175 190 \
+	--hide-extension "$APP_NAME.app" \
 	--app-drop-link 425 190 \
-	"MoonshineNoteTaker.dmg" \
-	"/Users/petewarden/Downloads/Moonshine Note Taker.app"
+	"$DMG_NAME" \
+	"$TMP_DIR/$APP_NAME.app"
+
+codesign --force --sign "$CERT_ID" "$DMG_NAME"
+xcrun notarytool submit "$DMG_NAME" \
+    --apple-id $APPLE_ID \
+    --team-id $DEV_TEAM_ID \
+    --password $APP_SPECIFIC_PASSWORD \
+    --wait
+xcrun stapler staple "$DMG_NAME"
+
+gsutil -h "Content-Type:application/x-apple-diskimage" -h "Content-Encoding:" \
+  cp "$DMG_NAME" "gs://download.moonshine.ai/apps/note-taker/$DMG_NAME"
+
+rm -rf "$TMP_DIR"
